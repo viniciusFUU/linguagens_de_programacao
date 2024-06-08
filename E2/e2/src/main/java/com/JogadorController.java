cef76f62ff49ce29859bb64e69bf455b289f4c60
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-06-08 19:18:54 -0300

    iniciando as listas

--- E2/e2/src/main/java/com/JogadorController.java
+++ E2/e2/src/main/java/com/JogadorController.java
@@ -1,19 +1,24 @@
 package com;
 
-import java.util.LinkedList;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.connection.DbConnection;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
 // import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
 public class JogadorController {
     @FXML
-    private List<Jogador> listaJogadores = new LinkedList<>(); 
+    private List<Jogador> listaJogadores = new ArrayList<>(); 
+    
+    @FXML
+    ListView<String> viewJogadores = new ListView<>();
 
     @FXML
     private TextField nomeField;
@@ -45,5 +50,29 @@ public class JogadorController {
         } finally {
             db.Desconnect();
         }
+    }
+
+    public ListView<String> readJogadores(){
+        DbConnection db = new DbConnection();
+
+        ResultSet resultListaJogadores = db.executeWithReturn("SELECT * FROM jogador");
+
+        try{
+            while (resultListaJogadores.next()) {
+                String nomeJogador = resultListaJogadores.getString("nome_jogador");
+                String posicaoJogador = resultListaJogadores.getString("posicao");
+                String clubeJogador = resultListaJogadores.getString("clube");
+
+                String detalhes = "'"+ nomeJogador +"' | '"+ posicaoJogador +"' | '"+ clubeJogador +"'";
+
+                viewJogadores.getItems().add(detalhes);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            db.Desconnect();
+        }
+
+        return viewJogadores;
     }
 }
