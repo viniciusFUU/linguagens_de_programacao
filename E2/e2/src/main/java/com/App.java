d80cbbef0a2901def7cc5ba41f76b510a82f1e5d
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-06-09 23:03:40 -0300

    Crud feito com sucesso

--- E2/e2/src/main/java/com/App.java
+++ E2/e2/src/main/java/com/App.java
@@ -1,8 +1,7 @@
 package com;
 
-import java.io.IOException;
-
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,25 +11,30 @@ public class App extends Application{
     private static Stage primaryStage;
 
     @Override
-    public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         primaryStage = stage;
         stage.setTitle("Cadastrar Jogador");
-        showPlayerScreen();
-        stage.show();
+        Platform.runLater(() -> {
+            try {
+                showPlayerScreen();
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    private static Parent loadFXML(String fxml) throws IOException{
+    private static Parent loadFXML(String fxml) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-
-        return fxmlLoader.load(); 
+        return fxmlLoader.load();
     }
 
-    public void showPlayerScreen() throws IOException{
+    public void showPlayerScreen() throws Exception {
         Scene scene = new Scene(loadFXML("playerScreen"));
         primaryStage.setScene(scene);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- E2/e2/src/main/java/com/JogadorController.java
+++ E2/e2/src/main/java/com/JogadorController.java
@@ -1,24 +1,30 @@
 package com;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.connection.DbConnection;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 // import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
 
-public class JogadorController {
+public class JogadorController{
     @FXML
     private List<Jogador> listaJogadores = new ArrayList<>(); 
     
     @FXML
-    ListView<String> viewJogadores = new ListView<>();
+    private ListView<String> viewJogadores = new ListView<>();
 
     @FXML
     private TextField nomeField;
@@ -28,6 +34,22 @@ public class JogadorController {
     
     @FXML
     private TextField clubeField;
+    
+    @FXML
+    private TextField nomeEdit;
+
+    @FXML
+    private TextField posicaoEdit;
+    
+    @FXML
+    private TextField clubeEdit;
+
+    private static String itens;
+
+    @FXML
+    private void initialize(){
+        readJogadores();
+    }
 
     public void createJogador(){
         DbConnection db = new DbConnection();
@@ -40,11 +62,16 @@ public class JogadorController {
         listaJogadores.add(jogador);
 
         try{
-            db.executeWithOutReturn("INSERT INTO jogador (nome_jogador, posicao, clube)"+
-             "VALUES ('"+ nome +"', '"+ posicao +"', '"+ clube +"')");
-
-             Alert alert = new Alert(AlertType.CONFIRMATION);
-             alert.show();
+            if (nome.isEmpty() || posicao.isEmpty() || clube.isEmpty()){
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.show();
+            } else {
+                db.executeWithOutReturn("INSERT INTO jogador (nome_jogador, posicao, clube)"+
+                 "VALUES ('"+ nome +"', '"+ posicao +"', '"+ clube +"')");
+    
+                 Alert alert = new Alert(AlertType.CONFIRMATION);
+                 alert.show();
+            }
         } catch (Exception e){
             e.printStackTrace();
         } finally {
@@ -52,20 +79,54 @@ public class JogadorController {
         }
     }
 
-    public ListView<String> readJogadores(){
-        DbConnection db = new DbConnection();
-
+    public void readJogadores(){
+        DbConnection db = new DbConnection();        
         ResultSet resultListaJogadores = db.executeWithReturn("SELECT * FROM jogador");
-
+        
         try{
             while (resultListaJogadores.next()) {
                 String nomeJogador = resultListaJogadores.getString("nome_jogador");
                 String posicaoJogador = resultListaJogadores.getString("posicao");
                 String clubeJogador = resultListaJogadores.getString("clube");
-
-                String detalhes = "'"+ nomeJogador +"' | '"+ posicaoJogador +"' | '"+ clubeJogador +"'";
+                
+                String detalhes = "Nome: " + nomeJogador + " | Posição: " + posicaoJogador + " | Clube: " + clubeJogador;
+                Jogador jogador = new Jogador(nomeJogador, posicaoJogador, clubeJogador);
 
                 viewJogadores.getItems().add(detalhes);
+                listaJogadores.add(jogador);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            db.Desconnect();
+        }
+    }
+
+    @FXML
+    public void updateJogador(){
+        if (itens == null){
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setContentText("Selecione um jogador para editar!");
+            alert.show();
+            return;
+        }
+
+        String nomeOriginal = itens.split(" | ")[1];
+        String nomeItem = nomeEdit.getText();
+        String posicaoItem = posicaoEdit.getText();
+        String clubeItem = clubeEdit.getText();
+
+        DbConnection db = new DbConnection();
+
+        try{
+            if(nomeItem != null && posicaoItem != null && clubeItem != null){
+                db.executeWithOutReturn("UPDATE jogador SET nome_jogador = '"+ nomeItem +"'," +
+                "posicao = '"+ posicaoItem +"'," + 
+                "clube = '"+ clubeItem +"'" + 
+                "WHERE nome_jogador = '"+ nomeOriginal +"'");
+    
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.show();
             }
         } catch (Exception e){
             e.printStackTrace();
@@ -73,6 +134,50 @@ public class JogadorController {
             db.Desconnect();
         }
 
-        return viewJogadores;
+        viewJogadores.refresh();
+    }
+
+    @FXML
+    public void deleteJogador(){
+        DbConnection db = new DbConnection();
+
+        itens = viewJogadores.getSelectionModel().getSelectedItem();
+        System.out.println(itens);
+
+        String nomeOriginal = itens.split(" | ")[1];
+        System.out.println(nomeOriginal);
+
+        try {
+            db.executeWithOutReturn("DELETE FROM jogador WHERE nome_jogador = '"+ nomeOriginal +"'");
+            
+            viewJogadores.getItems().remove(itens);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            db.Desconnect();
+        }
+    }
+
+    @FXML
+    private void openLista(ActionEvent event){
+        openFXML("screenListJogador.fxml");
+    }
+
+    @FXML
+    private void openEditJogador(ActionEvent event){
+        openFXML("screenEditJogador.fxml");
+
+        itens = viewJogadores.getSelectionModel().getSelectedItem();
+    }
+
+    private void openFXML(String fxmlFileName){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/" + fxmlFileName));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
